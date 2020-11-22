023ddc5c3b98852e60eb91371c9798ed7fc3ca45
cankarabey <58908373+cankarabey@users.noreply.github.com>
2020-11-22 18:43:52 +0100

    first version with exp model

--- ProjectAD CLI Version/src/com/company/Main.java
+++ ProjectAD CLI Version/src/com/company/Main.java
@@ -6,14 +6,40 @@ public class Main {
 
     public static void main(String[] args){
 
-        System.out.println("Please enter r - Value , N zero and time : ");
-        Scanner scanner = new Scanner(System.in);
-        double r = Double.valueOf(scanner.nextLine());
-        int n = Integer.parseInt(scanner.nextLine());
-        int t = Integer.parseInt(scanner.nextLine());
+        while (true) {
 
-        System.out.println("Deer population after t time : " + Model.calcDeer(r , n, t));
+            System.out.println("Please choose the type of animal : " +
+                    "\n 1. Cattle" +
+                    "\n 2. Deer" +
+                    "\n 3. Horse");
+            Scanner scanner = new Scanner(System.in);
+            int choice = Integer.parseInt(scanner.nextLine());
+            if (choice == 1) {
+                System.out.println("Please enter r - Value , N zero and time : ");
+                double r = Double.valueOf(scanner.nextLine());
+                int n = Integer.parseInt(scanner.nextLine());
+                int t = Integer.parseInt(scanner.nextLine());
 
+                System.out.println("Deer population after t time : " + Model.calcCattle(r, n, t));
+
+            } else if (choice == 2) {
+                System.out.println("Please enter r - Value , N zero and time : ");
+                double r = Double.valueOf(scanner.nextLine());
+                int n = Integer.parseInt(scanner.nextLine());
+                int t = Integer.parseInt(scanner.nextLine());
+
+                System.out.println("Deer population after t time : " + Model.calcDeer(r, n, t));
+            } else if (choice == 3) {
+                System.out.println("Please enter r - Value , N zero and time : ");
+                double r = Double.valueOf(scanner.nextLine());
+                int n = Integer.parseInt(scanner.nextLine());
+                int t = Integer.parseInt(scanner.nextLine());
+
+                System.out.println("Deer population after t time : " + Model.calcHorse(r, n, t));
+            } else {
+                System.out.println("Please enter a number between 1 and 3!");
+            }
+        }
 
     }
 }
--- ProjectAD CLI Version/src/com/company/Model.java
+++ ProjectAD CLI Version/src/com/company/Model.java
@@ -3,8 +3,24 @@ package com.company;
 public class Model {
 
     public static double calcDeer(double rValue , int nZero , int t){
-        double result = 0;
+        double result;
         result = nZero * Math.pow(Math.E , (rValue * t));
         return result;
     }
+
+    public static double calcHorse(double rValue , int nZero , int t){
+        double result;
+        result = nZero * Math.pow(Math.E , (rValue * t));
+        return result;
+    }
+
+    public static double calcCattle(double rValue , int nZero , int t){
+        double result;
+        result = nZero * Math.pow(Math.E , (rValue * t));
+        return result;
+    }
+
+
+
+
 }
