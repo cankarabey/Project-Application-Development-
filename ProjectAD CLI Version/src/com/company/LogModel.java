71f7565750bad1269e3b7ac85b851fe5e188ce22
cankarabey <58908373+cankarabey@users.noreply.github.com>
2020-11-25 11:37:51 +0100

    Create LogModel.java

---
+++ ProjectAD CLI Version/src/com/company/LogModel.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class LogModel {
+
+    private static final double rCattle = 0.001900165;
+    private static final double rHorse = 0.025769117;
+    private static final double rDeer = 0.029899109;
+    private static final int nZeroCattle = 293;
+    private static final int nZeroHorse = 964;
+    private static final int nZeroDeer = 1845;
+}
