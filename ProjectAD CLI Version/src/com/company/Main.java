ee5673410a2effaee677bc0575a479bb884a8eb2
Ivo Snijder <ivo.snijder@hotmail.com>
2020-12-03 13:44:59 +0100

    test for Roberts

--- ProjectAD CLI Version/src/com/company/Main.java
+++ ProjectAD CLI Version/src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args){
 
+    	//test for Roberts
 
         System.out.println("Cattle:\t r = 0,001900165 n0 2010 = 293\nHorse\t r = 0,025769117 n0 2010 = 964\nDeer:\t r = 0,029899109 n0 2010 = 1845\n");
 
