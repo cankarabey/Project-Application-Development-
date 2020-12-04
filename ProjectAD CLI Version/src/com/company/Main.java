d177894957d870ddf3cf00a6c171ed140aff363e
cankarabey <58908373+cankarabey@users.noreply.github.com>
2020-12-04 11:33:43 +0100

    pp

--- ProjectAD CLI Version/src/com/company/Main.java
+++ ProjectAD CLI Version/src/com/company/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
+        
         ExpModel exp = new ExpModel();
         exp.calculate(12, "CattleData.csv");
 
