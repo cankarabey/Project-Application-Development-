0a9056493970adf69cab23dafedac0c1cfe152a8
Ivo Snijder <ivo.snijder@hotmail.com>
2020-12-16 14:14:48 +0100

    Update PredationModel.java

--- ProjectAD CLI Version/src/com/company/PredationModel.java
+++ ProjectAD CLI Version/src/com/company/PredationModel.java
@@ -8,7 +8,16 @@ public class PredationModel implements IModel {
 
     @Override
     public TreeMap<Integer, Integer> calc(int t, String path) throws IOException {
-        return null;
+        int N0 = CSVReader.getN(path);
+        double h = 0.01685;
+        double D = 88052.197;
+        int ammountOfWolfs = 1;
+        for (int i = 1; i<=t; i++) {
+            int V =  (int) (CSVReader.calcR(path)*CSVReader.getN(path)*1 - (((1/h * (CSVReader.getN(path)^2)))/((CSVReader.getN(path)^2) + D))*ammountOfWolfs*1);
+            animals.put(i, V );
+            N0 = animals.get(i);
+        }
+        return animals;
     }
 
     //V= rVt-(kV^2/(V^2+D^2))pt
