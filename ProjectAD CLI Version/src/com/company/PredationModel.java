6f77864b200cbb68e7f2511d4bd844a115f15ac7
cankarabey <58908373+cankarabey@users.noreply.github.com>
2020-12-17 17:16:23 +0100

    small fix

--- ProjectAD CLI Version/src/com/company/PredationModel.java
+++ ProjectAD CLI Version/src/com/company/PredationModel.java
@@ -15,7 +15,7 @@ public class PredationModel implements IModel {
             double hDeer = 0.0243;
             double DDeer = 1;
             for (int i = 1; i <= t; i++) {
-                int V = (int) (CSVReader.calcR(path) * CSVReader.getN(path) * 1 - (((1 / hDeer * (CSVReader.getN(path) ^ 2))) / ((CSVReader.getN(path) ^ 2) + DDeer)) * amountOfWolfs * 1);
+                int V = (int) (CSVReader.calcR(path) * N0 * 1 - (((1 / hDeer * (N0 ^ 2))) / ((N0 ^ 2) + DDeer)) * amountOfWolfs * 1);
                 animals.put(i, V);
                 N0 = V;
             }
@@ -28,20 +28,20 @@ public class PredationModel implements IModel {
             double hHorse = 0.21;
             double DHorse = 1;
             for (int i = 1; i <= t; i++) {
-                int V = (int) (CSVReader.calcR(path) * CSVReader.getN(path) * 1 - (((1 / hHorse * (CSVReader.getN(path) ^ 2))) / ((CSVReader.getN(path) ^ 2) + DHorse)) * amountOfWolfs * 1);
+                int V = (int) (CSVReader.calcR(path) * N0 * 1 - (((1 / hHorse * (N0 ^ 2))) / ((N0 ^ 2) + DHorse)) * amountOfWolfs * 1);
                 animals.put(i, V);
                 N0 = V;
             }
             return animals;
         }
-        else if (path == "CattleData.cvs"){
+        else if (path == "CattleData.csv"){
             int N0 = CSVReader.getN(path);
             // int NWolfs = CSVReader.getN("Wolfs.cvs");
             int amountOfWolfs = 1;
             double hCattle = 0.366;
             double DCattle = 1;
             for (int i = 1; i <= t; i++) {
-                int V = (int) (CSVReader.calcR(path) * CSVReader.getN(path) * 1 - (((1 / hCattle * (CSVReader.getN(path) ^ 2))) / ((CSVReader.getN(path) ^ 2) + DCattle)) * amountOfWolfs * 1);
+                int V = (int) (CSVReader.calcR(path) * N0 * 1 - (((1 / hCattle * (N0 ^ 2))) / ((N0 ^ 2) + DCattle)) * amountOfWolfs * 1);
                 animals.put(i, V);
                 N0 = V;
             }
