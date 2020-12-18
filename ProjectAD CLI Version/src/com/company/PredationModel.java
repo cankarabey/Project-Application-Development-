cbe3071c6b0f7850adb60e7adb0fc1d85556d972
Lam544 <58904042+Lam544@users.noreply.github.com>
2020-12-18 12:44:30 +0100

    Small changes in predation model

--- ProjectAD CLI Version/src/com/company/PredationModel.java
+++ ProjectAD CLI Version/src/com/company/PredationModel.java
@@ -12,14 +12,15 @@ public class PredationModel implements IModel {
         if (path.equals("DeerData.csv")) {
             int N0 = CSVReader.getN(path);
             // int NWolfs = CSVReader.getN("Wolfs.cvs");
-            // int NWolfs = CSVReader.getN("Wolfs.csv");
             // int PopulationWolf = CSVReader.getN("Wolf.csv") * ((beta * (CSVReader.getN("DeerData.csv")) - CSVReader.getDeathRate("DeerData.csv")) * t; 
             int amountOfWolfs = 1;
-            double hDeer = 0.0243;
+          //Assuming a wolf eats 1:1:1 Deer:Horse:Cattle
+            double hDeer = 0.0243/3;
             double DDeer = (1 / (WolfAlfa * hDeer));
             double kDeer = (1 / hDeer);
             for (int i = 1; i <= t; i++) {
-                int V = (int) (CSVReader.calcR(path) * N0 * 1 - (((kDeer * (N0 ^ 2))) / ((N0 ^ 2) + ((int) DDeer ^ 2))) * amountOfWolfs * 1);
+                int V = (int) (CSVReader.calcR(path) * N0 * 1 - (((kDeer * (N0 ^ 2))) / ((N0 ^ 2) + 
+                		(DDeer * DDeer))) * amountOfWolfs * 1);
                 animals.put(i, V);
                 N0 = V;
             }
@@ -29,15 +30,16 @@ public class PredationModel implements IModel {
         }
         else if (path.equals("HorseData.csv")){
             int N0 = CSVReader.getN(path);
-            // int NWolfs = CSVReader.getN("Wolfs.cvs");
             // int NWolfs = CSVReader.getN("Wolfs.csv");
             // int PopulationWolf = CSVReader.getN("Wolf.csv") * ((beta * (CSVReader.getN("HorseData.csv")) - CSVReader.getDeathRate("HorseData.csv")) * t; 
             int amountOfWolfs = 1;
-            double hHorse = 0.21;
+          //Assuming a wolf eats 1:1:1 Deer:Horse:Cattle
+            double hHorse = 0.21/3;
             double DHorse = (1 / (WolfAlfa * hHorse));
             double kHorse = (1 / hHorse);
             for (int i = 1; i <= t; i++) {
-                int V = (int) (CSVReader.calcR(path) * N0 * 1 - (((kHorse * (N0 ^ 2))) / ((N0 ^ 2) + ((int) DHorse ^ 2))) * amountOfWolfs * 1);
+                int V = (int) (CSVReader.calcR(path) * N0 * 1 - (((kHorse * (N0 ^ 2))) / ((N0 ^ 2) + 
+                		(DHorse * DHorse))) * amountOfWolfs * 1);
                 animals.put(i, V);
                 N0 = V;
             }
@@ -46,16 +48,17 @@ public class PredationModel implements IModel {
 
         else if (path.equals("CattleData.csv")){
             int N0 = CSVReader.getN(path);
-            // int NWolfs = CSVReader.getN("Wolfs.cvs");
             // int NWolfs = CSVReader.getN("Wolfs.csv");
             // int PopulationWolf = CSVReader.getN("Wolf.csv") * ((beta * (CSVReader.getN("CattleData.csv")) - CSVReader.getDeathRate("CattleData.csv")) * t; 
             // int amountOfWolfs = PopulationWolf;
             int amountOfWolfs = 1;
-            double hCattle = 0.366;
+            //Assuming a wolf eats 1:1:1 Deer:Horse:Cattle
+            double hCattle = 0.366/3;
             double DCattle = (1 / (WolfAlfa * hCattle));
             double kCattle = (1 / hCattle);
             for (int i = 1; i <= t; i++) {
-                int V = (int) (CSVReader.calcR(path) * N0 * 1 - (((kCattle * (N0 ^ 2))) / ((N0 ^ 2) + ((int) DCattle ^ 2 ))) * amountOfWolfs * 1);
+                int V = (int) (CSVReader.calcR(path) * N0 * 1 - (((kCattle * (N0 ^ 2))) / ((N0 ^ 2) + 
+                		(DCattle * DCattle))) * amountOfWolfs * 1);
                 animals.put(i, V);
                 N0 = V;
             }
