88c8b8b0f76988e6a004869b117d2f5535285aeb
cankarabey <58908373+cankarabey@users.noreply.github.com>
2020-12-24 17:50:07 +0100

    added file choosing

--- ProjectAD/src/sample/importDataScreenController.java
+++ ProjectAD/src/sample/importDataScreenController.java
@@ -1,10 +1,17 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
 
 public class importDataScreenController {
 
+    private HashMap<String,FileReader> files = new HashMap<>();
     private boolean okClicked = false;
     private Stage dialogStage;
 
@@ -30,5 +37,62 @@ public class importDataScreenController {
     private void handleCancel() {
         dialogStage.close();
     }
+
+    @FXML
+    public FileReader getFile(){
+        try {
+            FileChooser fileChooser = new FileChooser();
+            FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("CSV files (*.csv)", "*.csv");
+            fileChooser.getExtensionFilters().add(extFilter);
+            File file = fileChooser.showOpenDialog(dialogStage);
+            return new FileReader(file);
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @FXML
+    public void handleChooseFileCD() {   //CD = cattle data
+        files.put("CattleData" , getFile());
+        System.out.println(files);
+    }
+    @FXML
+    public void handleChooseFileDD() {
+        files.put("DeerData" , getFile());
+        System.out.println(files);
+    }
+    @FXML
+    public void handleChooseFileHD() {
+        files.put("HorseData" , getFile());
+        System.out.println(files);
+    }
+    @FXML
+    public void handleChooseFileCHA() {
+        files.put("CattleHorseAvg" , getFile());
+        System.out.println(files);
+    }
+    @FXML
+    public void handleChooseFileCLT() {   //cattle life table
+        files.put("CattleLifeTable" , getFile());
+        System.out.println(files);
+    }
+    @FXML
+    public void handleChooseFileDLT() {
+        files.put("DeerLifeTable" , getFile());
+        System.out.println(files);
+    }
+    @FXML
+    public void handleChooseFileHLT() {
+        files.put("HorseLifeTable" , getFile());
+        System.out.println(files);
+    }
+    @FXML
+    public void handleChooseFileWD() {
+        files.put("Wolfs" , getFile());
+        System.out.println(files);
+    }
+
 
 }
