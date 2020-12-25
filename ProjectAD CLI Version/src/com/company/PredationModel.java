2c9f166af573e0d07e279cac6148550dbff43f7e
Lam544 <58904042+Lam544@users.noreply.github.com>
2020-12-25 12:59:20 +0100

    Update PredationModel.java

--- ProjectAD CLI Version/src/com/company/PredationModel.java
+++ ProjectAD CLI Version/src/com/company/PredationModel.java
@@ -9,58 +9,55 @@ public class PredationModel implements IModel {
 
     @Override
     public TreeMap<Integer, Integer> calc(int t, String path) throws IOException {
-        if (path.equals("DeerData.csv")) {
+        if (path.equals("CattleData.csv")) {
             int N0 = CSVReader.getN(path);
+            // int NWolfs = CSVReader.getN("Wolfs.csv");
+            // int PopulationWolf = CSVReader.getN("Wolf.csv") * ((beta * (CSVReader.getN("CattleData.csv")) - CSVReader.getDeathRate("CattleData.csv")) * t;
+            // int amountOfWolfs = PopulationWolf;
+            double r = CSVReader.calcR(path);
+            int amountOfWolfs = 1;
+            double hCattle = 0.366;
+            double DCattle = (1 / (WolfAlfa * hCattle));
+            double kCattle = (1 / hCattle);
+            for (int i = 1; i <= t; i++) {
+                int V = (int) (r * N0 * i - (((kCattle * (N0 ^ 2))) / ((N0 ^ 2) +
+                        (DCattle * DCattle))) * amountOfWolfs * i);
+                int A = N0 - V;
+                animals.put(i, A);
+
+            }
+            return animals;
+        }
+        else if (path.equals("DeerData.csv")) {
+            int N0 = CSVReader.getN(path);
+            double r = CSVReader.calcR(path);
             // int NWolfs = CSVReader.getN("Wolfs.cvs");
             // int PopulationWolf = CSVReader.getN("Wolf.csv") * ((beta * (CSVReader.getN("DeerData.csv")) - CSVReader.getDeathRate("DeerData.csv")) * t; 
             int amountOfWolfs = 1;
-          //Assuming a wolf eats 1:1:1 Deer:Horse:Cattle
-            double hDeer = 0.0243/3;
+            double hDeer = 0.0243;
             double DDeer = (1 / (WolfAlfa * hDeer));
             double kDeer = (1 / hDeer);
             for (int i = 1; i <= t; i++) {
-                int V = (int) (CSVReader.calcR(path) * N0 * 1 - (((kDeer * (N0 ^ 2))) / ((N0 ^ 2) + 
-                		(DDeer * DDeer))) * amountOfWolfs * 1);
-                animals.put(i, V);
-                N0 = V;
+                int V = (int) ((r * N0 * i) - ((kDeer * (N0 ^ 2)) / ((N0 ^ 2) + (DDeer * DDeer))) * amountOfWolfs * i);
+                int A = N0 - V;
+                animals.put(i,A);
             }
             return animals;
-            
-            
         }
         else if (path.equals("HorseData.csv")){
             int N0 = CSVReader.getN(path);
             // int NWolfs = CSVReader.getN("Wolfs.csv");
             // int PopulationWolf = CSVReader.getN("Wolf.csv") * ((beta * (CSVReader.getN("HorseData.csv")) - CSVReader.getDeathRate("HorseData.csv")) * t; 
+            double r = CSVReader.calcR(path);
             int amountOfWolfs = 1;
-          //Assuming a wolf eats 1:1:1 Deer:Horse:Cattle
-            double hHorse = 0.21/3;
+            double hHorse = 0.21;
             double DHorse = (1 / (WolfAlfa * hHorse));
             double kHorse = (1 / hHorse);
             for (int i = 1; i <= t; i++) {
-                int V = (int) (CSVReader.calcR(path) * N0 * 1 - (((kHorse * (N0 ^ 2))) / ((N0 ^ 2) + 
-                		(DHorse * DHorse))) * amountOfWolfs * 1);
-                animals.put(i, V);
-                N0 = V;
-            }
-            return animals;
-        }
-
-        else if (path.equals("CattleData.csv")){
-            int N0 = CSVReader.getN(path);
-            // int NWolfs = CSVReader.getN("Wolfs.csv");
-            // int PopulationWolf = CSVReader.getN("Wolf.csv") * ((beta * (CSVReader.getN("CattleData.csv")) - CSVReader.getDeathRate("CattleData.csv")) * t; 
-            // int amountOfWolfs = PopulationWolf;
-            int amountOfWolfs = 1;
-            //Assuming a wolf eats 1:1:1 Deer:Horse:Cattle
-            double hCattle = 0.366/3;
-            double DCattle = (1 / (WolfAlfa * hCattle));
-            double kCattle = (1 / hCattle);
-            for (int i = 1; i <= t; i++) {
-                int V = (int) (CSVReader.calcR(path) * N0 * 1 - (((kCattle * (N0 ^ 2))) / ((N0 ^ 2) + 
-                		(DCattle * DCattle))) * amountOfWolfs * 1);
-                animals.put(i, V);
-                N0 = V;
+                int V = (int) (r * N0 * i - (((kHorse * (N0 ^ 2))) / ((N0 ^ 2) +
+                		(DHorse * DHorse))) * amountOfWolfs * i);
+                int A = N0 - V;
+                animals.put(i,A);
             }
             return animals;
         }
