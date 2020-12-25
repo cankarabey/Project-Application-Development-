d1c7d07940e251cf3ed6d40b6b6933763dee82a5
Lam544 <58904042+Lam544@users.noreply.github.com>
2020-12-25 13:05:46 +0100

    Update PredationModel.java

--- ProjectAD CLI Version/src/com/company/PredationModel.java
+++ ProjectAD CLI Version/src/com/company/PredationModel.java
@@ -6,22 +6,21 @@ import java.util.TreeMap;
 public class PredationModel implements IModel {
     static TreeMap<Integer, Integer> animals = new TreeMap<>();
     double WolfAlfa = 0.2;
+    // int amountOfWolfs = PopulationWolf;
+    // int NWolfs = CSVReader.getN("Wolfs.cvs");
+    // int PopulationWolf = CSVReader.getN("Wolf.csv") * ((beta * (CSVReader.getN("DeerData.csv")) - CSVReader.getDeathRate("DeerData.csv")) * t;
+    int amountOfWolfs = 1;
 
     @Override
     public TreeMap<Integer, Integer> calc(int t, String path) throws IOException {
         if (path.equals("CattleData.csv")) {
             int N0 = CSVReader.getN(path);
-            // int NWolfs = CSVReader.getN("Wolfs.csv");
-            // int PopulationWolf = CSVReader.getN("Wolf.csv") * ((beta * (CSVReader.getN("CattleData.csv")) - CSVReader.getDeathRate("CattleData.csv")) * t;
-            // int amountOfWolfs = PopulationWolf;
             double r = CSVReader.calcR(path);
-            int amountOfWolfs = 1;
             double hCattle = 0.366;
             double DCattle = (1 / (WolfAlfa * hCattle));
             double kCattle = (1 / hCattle);
             for (int i = 1; i <= t; i++) {
-                int V = (int) (r * N0 * i - (((kCattle * (N0 ^ 2))) / ((N0 ^ 2) +
-                        (DCattle * DCattle))) * amountOfWolfs * i);
+                int V = (int) (r * N0 * i - (((kCattle * (N0 ^ 2))) / ((N0 ^ 2) + (DCattle * DCattle))) * amountOfWolfs * i) ;
                 int A = N0 - V;
                 animals.put(i, A);
 
@@ -31,14 +30,11 @@ public class PredationModel implements IModel {
         else if (path.equals("DeerData.csv")) {
             int N0 = CSVReader.getN(path);
             double r = CSVReader.calcR(path);
-            // int NWolfs = CSVReader.getN("Wolfs.cvs");
-            // int PopulationWolf = CSVReader.getN("Wolf.csv") * ((beta * (CSVReader.getN("DeerData.csv")) - CSVReader.getDeathRate("DeerData.csv")) * t; 
-            int amountOfWolfs = 1;
             double hDeer = 0.0243;
             double DDeer = (1 / (WolfAlfa * hDeer));
             double kDeer = (1 / hDeer);
             for (int i = 1; i <= t; i++) {
-                int V = (int) ((r * N0 * i) - ((kDeer * (N0 ^ 2)) / ((N0 ^ 2) + (DDeer * DDeer))) * amountOfWolfs * i);
+                int V = (int) ((r * N0 * i) - ((kDeer * (N0 ^ 2)) / ((N0 ^ 2) + (DDeer * DDeer))) * amountOfWolfs * i) ;
                 int A = N0 - V;
                 animals.put(i,A);
             }
@@ -46,10 +42,7 @@ public class PredationModel implements IModel {
         }
         else if (path.equals("HorseData.csv")){
             int N0 = CSVReader.getN(path);
-            // int NWolfs = CSVReader.getN("Wolfs.csv");
-            // int PopulationWolf = CSVReader.getN("Wolf.csv") * ((beta * (CSVReader.getN("HorseData.csv")) - CSVReader.getDeathRate("HorseData.csv")) * t; 
             double r = CSVReader.calcR(path);
-            int amountOfWolfs = 1;
             double hHorse = 0.21;
             double DHorse = (1 / (WolfAlfa * hHorse));
             double kHorse = (1 / hHorse);
