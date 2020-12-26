9c9da9e44da34c36c43c817408dd50cff8d645c3
RobertsHDMI <58832132+RobertsHDMI@users.noreply.github.com>
2020-12-27 01:02:29 +0200

    hooked each tab with inner screen

---
+++ ProjectAD/src/sample/Controllers/innerScreenController.java
@@ -0,0 +1,14 @@
+package sample.Controllers;
+
+import javafx.fxml.FXML;
+
+public class innerScreenController {
+
+    @FXML private exponentialTabController exponentialTabController;
+    @FXML private logisticTabController logisticTabController;
+    @FXML private ageTabController ageTabController;
+    @FXML private competitiveTabController competitiveTabController;
+    @FXML private predationTabController predationTabController;
+
+
+}
