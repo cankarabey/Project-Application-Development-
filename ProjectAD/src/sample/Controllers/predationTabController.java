0b9f20016b48e0926a791f7bd8ebedeae0ba5849
cankarabey <58908373+cankarabey@users.noreply.github.com>
2021-01-05 17:55:52 +0100

    started with predation tab

--- ProjectAD/src/sample/Controllers/predationTabController.java
+++ ProjectAD/src/sample/Controllers/predationTabController.java
@@ -1,9 +1,36 @@
 package sample.Controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Tab;
+import javafx.scene.control.*;
+import util.Predictions;
 
 public class predationTabController {
 
     @FXML private Tab predation;
+    @FXML private TextField rValCattle;
+    @FXML private TextField rValHorse;
+    @FXML private TextField rValDeer;
+    @FXML private TextField nValCattle;
+    @FXML private TextField nValHorse;
+    @FXML private TextField nValDeer;
+    @FXML private TextField timeValue;
+    @FXML private TextField kValCattle;
+    @FXML private TextField kValHorse;
+    @FXML private TextField kValDeer;
+    @FXML private TextField dValCattle;
+    @FXML private TextField dValHorse;
+    @FXML private TextField dValDeer;
+    @FXML private TextField hValCattle;
+    @FXML private TextField hValHorse;
+    @FXML private TextField hValDeer;
+    @FXML private CheckBox checkBoxPie;
+    @FXML private CheckBox checkBoxLine;
+    @FXML private TableView<Predictions> tableView;
+    @FXML private TableColumn<Predictions , Integer> year;
+    @FXML private TableColumn<Predictions , Integer> cattleNumber;
+    @FXML private TableColumn<Predictions , Integer> deerNumber;
+    @FXML private TableColumn<Predictions , Integer> horseNumber;
+    private ObservableList<Predictions> predictions = FXCollections.observableArrayList();
 }
