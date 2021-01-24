8cefe1c36bfee075612f1dc024f929211dd87f34
Yasin <agentyinster101@gmail.com>
2021-01-24 23:29:05 +0400

    Added user manual to app

--- ProjectAD/src/sample/Controllers/helpScreenController.java
+++ ProjectAD/src/sample/Controllers/helpScreenController.java
@@ -1,15 +1,30 @@
 package sample.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
 import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class helpScreenController {
     private boolean okClicked = false;
     private Stage dialogStage;
+    @FXML private Hyperlink userManual;
 
     @FXML
-    private void initialize() {
+    private void initialize(){}
+
+    public void openManual() {
+        try{
+            File file = new File("src"+File.separator+"Data/UserManualApplicationDevelopment.pdf");
+            Desktop.getDesktop().open(file);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
+
 
     public void setDialogStage(Stage dialogStage) {
         this.dialogStage = dialogStage;
